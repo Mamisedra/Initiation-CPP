0ccf84f4e2800ab524a3415c785e28d52d8c3c9c
Mamisedra Hariniaina Ranaivoson <mranaivo@c1r9s5.42antananarivo.mg>
2024-12-16 17:37:52 +0300

    zombie ex00 finish

--- cpp01/ex00/includes/Zombie.hpp
+++ cpp01/ex00/includes/Zombie.hpp
@@ -6,7 +6,30 @@
 /*   By: mranaivo <mranaivo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 17:00:17 by mranaivo          #+#    #+#             */
-/*   Updated: 2024/12/16 17:00:18 by mranaivo         ###   ########.fr       */
+/*   Updated: 2024/12/16 17:20:47 by mranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef  __ZOMBIE_H__
+# define __ZOMBIE_H__
+
+#include <string>
+#include <iostream>
+
+class	Zombie
+{
+	public:
+		void	announce( void );
+		Zombie( void );
+		Zombie( std::string name );
+		~Zombie( void );
+
+	private:
+		std::string	_name;
+
+};
+
+Zombie*	newZombie( std::string name );
+void	randomChump( std::string name );
+
+# endif
--- cpp01/ex00/srcs/newZombie.cpp
+++ cpp01/ex00/srcs/newZombie.cpp
@@ -6,7 +6,16 @@
 /*   By: mranaivo <mranaivo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 17:00:08 by mranaivo          #+#    #+#             */
-/*   Updated: 2024/12/16 17:00:09 by mranaivo         ###   ########.fr       */
+/*   Updated: 2024/12/16 17:26:42 by mranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Zombie.hpp"
+
+Zombie*	newZombie( std::string name )
+{
+	Zombie*	new_zombie;
+
+	new_zombie = new Zombie(name);
+	return (new_zombie);
+}

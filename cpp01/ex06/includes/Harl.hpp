3398a3a2a508832359c762071d76de6d90d25f92
Mamisedra Hariniaina Ranaivoson <mranaivo@c1r7s12.42antananarivo.mg>
2025-01-13 15:16:15 +0300

    add new

--- cpp01/ex06/includes/Harl.hpp
+++ cpp01/ex06/includes/Harl.hpp
@@ -6,7 +6,27 @@
 /*   By: mranaivo <mranaivo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 16:59:33 by mranaivo          #+#    #+#             */
-/*   Updated: 2024/12/16 16:59:34 by mranaivo         ###   ########.fr       */
+/*   Updated: 2025/01/13 14:56:44 by mranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef __HARL_H__
+# define __HARL_H__
+
+# include <iostream>
+
+class	Harl
+{
+	public :
+		Harl( void );
+		~Harl( void );
+		void	complain( std::string level);
+
+	private :
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+};
+
+#endif
